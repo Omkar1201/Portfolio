f8b36ed023584f8646e8f7f1f3dba367d47bf92f
Omkar <omkarsalunkhe3597@gmail.com>
2024-07-21 20:18:17 +0530

    add another project

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -3,15 +3,16 @@ import { FaGithub } from 'react-icons/fa';
 import knowledge from '../images/knowledgenest.jpg'
 import newsi from '../images/newsi.jpg'
 import gifimg from '../images/gif.png'
+import coursesimg from '../images/courses.png'
 import { motion } from 'framer-motion';
 import { FiEye } from "react-icons/fi";
 export default function Portfolio() {
     return (
         <div>
             <motion.div
-            initial={{ opacity: 0}}
-            animate={{ opacity: 1}}
-            transition={{ duration: 0.8 }}
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                transition={{ duration: 0.8 }}
             >
                 <h2 className='text-[1.8rem] font-semibold'>Portfolio</h2>
                 <div className='border-[0.15rem] rounded-full border-cyan-500 w-10 my-2'></div>
@@ -60,6 +61,26 @@ export default function Portfolio() {
                         Implemented real-time news updates from API
                         Utilized RESTful APIs to fetch and display news articles
                         Enabled users to search various news based their interests.
+                    </div>
+                </motion.div>
+                <motion.div
+                    initial={{ opacity: 0, scale: 0.5 }}
+                    animate={{ opacity: 1, scale: 1 }}
+                    transition={{ duration: 0.2, delay: 0.2 }}
+                    className='flex flex-col gap-2 w-[22rem] border border-white border-opacity-30 '>
+                    <a href='https://omkar1201.github.io/MiniReact_project/' rel='noreferrer' target='_blank' className=' h-[14rem] overflow-hidden relative group cursor-pointer'>
+                        <img className='p-1 duration-[0.5s] group-hover:scale-105' src={coursesimg} alt="courses_photo" />
+                        <div className='text-[1.5rem] absolute top-[40%] group-hover:opacity-100 z-10 bg-black p-2 right-[44%] rounded-xl opacity-0 text-cyan-500'><FiEye /></div>
+                    </a>
+
+                    <a href='https://github.com/Omkar1201/MiniReact_project' target='_blank' rel="noreferrer" className='flex px-2 hover:text-opacity-100 text-white text-opacity-70 duration-[0.4s] items-center gap-2 my-2'>
+                        <div className='text-[1.5rem]'><FaGithub /></div>
+                        <div className=' font-semibold'>
+                            Mini React project
+                        </div>
+                    </a>
+                    <div className='font-thin text-white text-opacity-80 px-2'>
+                        Designed and developed a responsive frontend application to showcase a variety of courses. The project features a user-friendly interface for browsing courses.
                     </div>
                 </motion.div>
                 <motion.div
