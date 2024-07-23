ff9d6fa3403d820e9f38bf94ae2410c7ca3f7e2f
Omkar <omkarsalunkhe3597@gmail.com>
2024-07-23 17:46:57 +0530

    strcuture changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import { Route, Routes } from "react-router-dom";
 import Me from "./components/Me";
-import Navbar from "./components/Navbar";
-import About from "./components/About";
-import Resume from "./components/Resume";
-import Portfolio from "./components/Portfolio";
-import Contact from "./components/Contact";
+
 import './App.css'
+import Home from "./components/Home";
 function App() {
 	return (
 		<div className="App flex gap-12 bg-black p-16">
 
-			<Navbar />
 			<Me />
-			<div className='border p-6 w-full rounded-3xl bg-white bg-opacity-5 border-opacity-10 border-white h-[60rem text-white'>
-				<Routes>
-					<Route path='/' element={<About/>}></Route>
-					<Route path='/resume' element={<Resume/>}></Route>
-					<Route path='/projects' element={<Portfolio/>}></Route>
-					<Route path='/contact' element={<Contact/>}></Route>
-				</Routes>
-			</div>
+			<Home/>
+			
 
 
 		</div>
---
+++ src/components/Home.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { Route, Routes } from "react-router-dom";
+import About from "./About";
+import Resume from "./Resume";
+import Portfolio from "./Portfolio";
+import Contact from "./Contact";
+import Navbar from './Navbar';
+export default function Home() {
+    return (
+        <div className='border w-full rounded-3xl bg-white bg-opacity-5 border-opacity-10 border-white text-white'>
+            <div className=' flex justify-end'>
+                <Navbar />
+            </div>
+            <div className='px-6'>
+                <Routes>
+                    <Route path='/' element={<About />}></Route>
+                    <Route path='/resume' element={<Resume />}></Route>
+                    <Route path='/projects' element={<Portfolio />}></Route>
+                    <Route path='/contact' element={<Contact />}></Route>
+                </Routes>
+            </div>
+        </div>
+    )
+}
